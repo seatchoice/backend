f2b722b067b0cc73b6ad217183cba2d233063ff2
junga970 <junga2940@gmail.com>
2023-02-03 03:00:41 +0900

    Initial commit

---
+++ src/main/java/com/example/seatchoice/SeatchoiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.seatchoice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeatchoiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeatchoiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/seatchoice/SeatchoiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.seatchoice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeatchoiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
