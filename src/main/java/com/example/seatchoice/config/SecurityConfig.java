b90284441696351be5cb5aee38629fcab05ca7fc
YJ <zerosix1017@gmail.com>
2023-02-11 15:34:02 +0900

    chore : gradle 캐싱 다시시도

--- src/main/java/com/example/seatchoice/config/SecurityConfig.java
+++ src/main/java/com/example/seatchoice/config/SecurityConfig.java
@@ -64,13 +64,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.authenticationEntryPoint(new RestAuthenticationEntryPoint())
 			.accessDeniedHandler(tokenAccessDeniedHandler)
 			.and()
-
 			.authorizeRequests()
 			.requestMatchers(CorsUtils::isPreFlightRequest).permitAll()
 			.antMatchers("/api/**").hasAnyAuthority(MemberRole.USER.getCode())
 			.antMatchers("/api/**/admin/**").hasAnyAuthority(MemberRole.ADMIN.getCode())
 			.anyRequest().authenticated()
-
 			.and()
 			.oauth2Login()
 			.authorizationEndpoint()
