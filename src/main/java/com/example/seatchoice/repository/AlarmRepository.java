00d96488ad9c26351f8ac06f1cee8bd66c27658c
YJ <zerosix1017@gmail.com>
2023-02-12 01:03:10 +0900

    chore : repository annotation 추가

--- src/main/java/com/example/seatchoice/repository/AlarmRepository.java
+++ src/main/java/com/example/seatchoice/repository/AlarmRepository.java
@@ -5,7 +5,9 @@ import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AlarmRepository extends JpaRepository<Alarm, Long> {
 
     Page<Alarm> findByMemberId(Long memberId, Pageable pageable);
