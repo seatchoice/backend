ef57e8af774042cabe7b840d34d57fbb3a4c5a92
YJ <zerosix1017@gmail.com>
2023-02-11 12:43:44 +0900

    feat : oauth2 로그인을 위한 기본 세팅

--- src/main/java/com/example/seatchoice/SeatchoiceApplication.java
+++ src/main/java/com/example/seatchoice/SeatchoiceApplication.java
@@ -1,11 +1,15 @@
 package com.example.seatchoice;
 
+import com.example.seatchoice.config.AppProperties;
+import com.example.seatchoice.config.CorsProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients
 @SpringBootApplication
+@EnableConfigurationProperties({CorsProperties.class, AppProperties.class})
 public class SeatchoiceApplication {
 
 	public static void main(String[] args) {
