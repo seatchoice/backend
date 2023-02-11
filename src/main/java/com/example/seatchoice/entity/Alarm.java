8199c497d5561f1f8b822ea1e62d321a5a5f9a77
YJ <zerosix1017@gmail.com>
2023-02-12 00:00:46 +0900

    fix : custom exception 출력 안되는 문제 해결

--- src/main/java/com/example/seatchoice/entity/Alarm.java
+++ src/main/java/com/example/seatchoice/entity/Alarm.java
@@ -13,8 +13,10 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
@@ -32,5 +34,5 @@ public class Alarm extends BaseEntity {
 	private String url;
 
 	@NotNull
-	private boolean checkAlarm;
+	private Boolean checkAlarm;
 }
