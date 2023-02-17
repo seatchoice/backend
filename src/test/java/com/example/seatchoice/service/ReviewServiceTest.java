2b052ffdfe232d6113fb7116609ac3a9b69a262f
sunju5402 <ssonju5402@gmail.com>
2023-02-17 23:04:11 +0900

    test : 리뷰 생성 실패 - 리뷰 존재하지 않는 경우 테스트 코드 추가

--- src/test/java/com/example/seatchoice/service/ReviewServiceTest.java
+++ src/test/java/com/example/seatchoice/service/ReviewServiceTest.java
@@ -1,6 +1,7 @@
 package com.example.seatchoice.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
@@ -9,16 +10,23 @@ import static org.mockito.Mockito.verify;
 
 import com.example.seatchoice.dto.cond.ReviewCond;
 import com.example.seatchoice.dto.param.ReviewParam;
+import com.example.seatchoice.entity.Image;
 import com.example.seatchoice.entity.Member;
 import com.example.seatchoice.entity.Review;
 import com.example.seatchoice.entity.Theater;
 import com.example.seatchoice.entity.TheaterSeat;
+import com.example.seatchoice.exception.CustomException;
 import com.example.seatchoice.repository.ImageRepository;
 import com.example.seatchoice.repository.MemberRepository;
 import com.example.seatchoice.repository.ReviewLikeRepository;
 import com.example.seatchoice.repository.ReviewRepository;
 import com.example.seatchoice.repository.TheaterRepository;
 import com.example.seatchoice.repository.TheaterSeatRepository;
+import com.example.seatchoice.type.ErrorCode;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -29,6 +37,7 @@ import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -104,5 +113,45 @@ public class ReviewServiceTest {
 		verify(reviewRepository, times(1)).save(captor.capture());
 		assertEquals(4, reviewCond.getRating());
 		assertEquals("좌석 괜찮습니다!", reviewCond.getContent());
+		assertEquals(null, reviewCond.getImages());
+	}
+
+	@Test
+	@DisplayName("리뷰 생성 실패 - 리뷰 존재하지 않는 경우")
+	void createReview_notFountSeat() {
+	    // given
+		Member member = new Member();
+		Theater theater = new Theater();
+		theater.setId(1L);
+		List<TheaterSeat> theaterSeats = Arrays.asList(
+			TheaterSeat.builder()
+				.theater(theater)
+				.floor(1)
+				.section("OP")
+				.seatRow("1")
+				.number(2)
+				.rating(0.0)
+				.reviewAmount(0L)
+				.build(),
+			TheaterSeat.builder()
+				.theater(theater)
+				.floor(2)
+				.section("OP")
+				.seatRow("1")
+				.number(1)
+				.rating(0.0)
+				.reviewAmount(0L)
+				.build()
+		);
+		ReviewParam request = new ReviewParam(1, "OP", "1", 1, "좌석 괜찮습니다!", 4);
+
+		given(theaterSeatRepository.findAllByTheaterId(anyLong())).willReturn(theaterSeats);
+
+		// when
+		CustomException customException = assertThrows(CustomException.class,
+			() -> reviewService.createReview(7L, 562L, null, request));
+
+		// then
+		assertEquals(customException.getErrorCode(), ErrorCode.NOT_FOUND_SEAT);
 	}
 }
