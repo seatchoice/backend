9699abe7c8e423ae9271a6b5c69e14586edfff7c
sunju5402 <ssonju5402@gmail.com>
2023-02-17 18:28:31 +0900

    test : 이미지 파일 없는 리뷰 등록 테스트 구현

--- src/main/java/com/example/seatchoice/dto/param/ReviewParam.java
+++ src/main/java/com/example/seatchoice/dto/param/ReviewParam.java
@@ -2,8 +2,8 @@ package com.example.seatchoice.dto.param;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Positive;
+import javax.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -28,7 +28,7 @@ public class ReviewParam {
 	private Integer seatNumber;
 
 	@NotBlank(message = "필수 입력입니다.")
-	@Pattern(regexp = "^.{1,300}$", message = "리뷰내용은 1~300자로 내로 입력하세요.")
+	@Size(min = 1, max = 300, message = "리뷰내용은 1~300자로 내로 입력하세요.")
 	private String content;
 
 	private Integer rating;
