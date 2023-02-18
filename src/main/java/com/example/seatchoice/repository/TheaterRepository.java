f98cf74987742da4844f1098fe7fddbb6c59bff2
reeruryu <h2ju1004@gmail.com>
2023-02-18 18:49:05 +0900

    feat : 공연장 목록 조회 구현
    - 테스트 완료

--- src/main/java/com/example/seatchoice/repository/TheaterRepository.java
+++ src/main/java/com/example/seatchoice/repository/TheaterRepository.java
@@ -1,6 +1,8 @@
 package com.example.seatchoice.repository;
 
+import com.example.seatchoice.entity.Facility;
 import com.example.seatchoice.entity.Theater;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +10,7 @@ import org.springframework.stereotype.Repository;
 public interface TheaterRepository extends JpaRepository<Theater, Long> {
 
 	Theater findByNameAndFacility_Name(String name, String facilityName);
+
+	List<Theater> findAllByFacility(Facility facility);
 
 }
