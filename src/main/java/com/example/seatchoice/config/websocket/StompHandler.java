af3e75fa78ee3747f34ed99135cb55757d2f15bc
YJ <zerosix1017@gmail.com>
2023-02-21 21:52:37 +0900

    chore : 토큰 관련 수정

--- src/main/java/com/example/seatchoice/config/websocket/StompHandler.java
+++ src/main/java/com/example/seatchoice/config/websocket/StompHandler.java
@@ -1,11 +1,15 @@
 package com.example.seatchoice.config.websocket;
 
 
+import static com.example.seatchoice.type.ErrorCode.EMPTY_TOKEN;
 import static com.example.seatchoice.type.ErrorCode.INVALID_TOKEN;
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.UNAUTHORIZED;
 
 import com.example.seatchoice.exception.CustomException;
 import com.example.seatchoice.service.auth.TokenService;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.SignatureException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.Ordered;
@@ -15,6 +19,8 @@ import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.simp.stomp.StompCommand;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
 @Slf4j
@@ -30,9 +36,20 @@ public class StompHandler implements ChannelInterceptor {
         StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(message);
 
         if (StompCommand.CONNECT.equals(headerAccessor.getCommand())) {
-            String token = headerAccessor.getFirstNativeHeader("Authorization");
-            if (!tokenService.validateToken(token)) {
-                throw new CustomException(INVALID_TOKEN, BAD_REQUEST);
+
+            try {
+                String token = headerAccessor.getFirstNativeHeader("Authorization");
+                if (tokenService.validateToken(token)) {
+                    Authentication authentication = tokenService.getAuthentication(token);
+                    SecurityContextHolder.getContext().setAuthentication(authentication);
+                } else {
+                    throw new CustomException(INVALID_TOKEN, UNAUTHORIZED);
+                }
+
+            } catch (SignatureException | MalformedJwtException e) {
+                throw new CustomException(INVALID_TOKEN, UNAUTHORIZED);
+            } catch (IllegalArgumentException e) {
+                throw new CustomException(EMPTY_TOKEN, BAD_REQUEST);
             }
         }
 
