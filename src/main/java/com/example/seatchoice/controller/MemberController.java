585b6cbe907941ba3842913d05be1860d6bffcd6
junga970 <junga2940@gmail.com>
2023-02-22 21:24:59 +0900

    feat : access-token 재발급 기능 추가

--- src/main/java/com/example/seatchoice/controller/MemberController.java
+++ src/main/java/com/example/seatchoice/controller/MemberController.java
@@ -6,11 +6,13 @@ import com.example.seatchoice.service.MemberService;
 import com.example.seatchoice.service.auth.TokenService;
 import java.io.IOException;
 import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.json.simple.parser.ParseException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,7 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/api/oauth/{provider}")
+@RequestMapping("/api")
 public class MemberController {
 
 	private final MemberService memberService;
@@ -27,7 +29,7 @@ public class MemberController {
 
 	private final String REFRESH_TOKEN_KEY = "refreshToken";
 
-	@PostMapping("/login")
+	@PostMapping("/oauth/{provider}/login")
 	public ResponseEntity<Void>  oauthLogin(
 		@RequestParam String code, @PathVariable("provider") String provider, HttpServletResponse response)
 		throws IOException, ParseException {
@@ -43,13 +45,21 @@ public class MemberController {
 		return ResponseEntity.ok().build();
 	}
 
-	@PostMapping("/logout")
+	@PostMapping("/oauth/{provider}/logout")
 	public ResponseEntity<Void> oauthLogin(
 		@AuthenticationPrincipal Member member, @PathVariable("provider") String provider, HttpServletResponse response)
 		throws IOException {
 
 		memberService.oauthLogout(member.getId(), provider);
 		tokenService.resetHeader(response);
+
+		return ResponseEntity.ok().build();
+	}
+
+	@GetMapping("/auth/tokens")
+	public ResponseEntity<Void> reissueAccessToken(HttpServletRequest request, HttpServletResponse response) {
+		String accessToken = tokenService.reissueAccessToken(request, response);
+		response.setHeader("Authorization", accessToken);
 
 		return ResponseEntity.ok().build();
 	}
