87deb3e3531bc5ab124fdf987a6e5120446f9d7a
YJ <zerosix1017@gmail.com>
2023-02-22 18:22:35 +0900

    chore : 유저 검증 방식 변경

--- src/main/java/com/example/seatchoice/config/websocket/StompHandler.java
+++ src/main/java/com/example/seatchoice/config/websocket/StompHandler.java
@@ -3,10 +3,13 @@ package com.example.seatchoice.config.websocket;
 
 import static com.example.seatchoice.type.ErrorCode.EMPTY_TOKEN;
 import static com.example.seatchoice.type.ErrorCode.INVALID_TOKEN;
+import static com.example.seatchoice.type.ErrorCode.NOT_FOUND_MEMBER;
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
 import static org.springframework.http.HttpStatus.UNAUTHORIZED;
 
 import com.example.seatchoice.exception.CustomException;
+import com.example.seatchoice.repository.MemberRepository;
 import com.example.seatchoice.service.auth.TokenService;
 import io.jsonwebtoken.MalformedJwtException;
 import io.jsonwebtoken.SignatureException;
@@ -19,8 +22,6 @@ import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.simp.stomp.StompCommand;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.messaging.support.ChannelInterceptor;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
 @Slf4j
@@ -30,6 +31,7 @@ import org.springframework.stereotype.Component;
 public class StompHandler implements ChannelInterceptor {
 
     private final TokenService tokenService;
+    private final MemberRepository memberRepository;
 
     @Override
     public Message<?> preSend(Message<?> message, MessageChannel channel) {
@@ -40,8 +42,9 @@ public class StompHandler implements ChannelInterceptor {
             try {
                 String token = headerAccessor.getFirstNativeHeader("Authorization");
                 if (tokenService.validateToken(token)) {
-                    Authentication authentication = tokenService.getAuthentication(token);
-                    SecurityContextHolder.getContext().setAuthentication(authentication);
+                    Long memberId = tokenService.getMemberId(token);
+                    memberRepository.findById(memberId).orElseThrow(
+                        () -> new CustomException(NOT_FOUND_MEMBER, NOT_FOUND));
                 } else {
                     throw new CustomException(INVALID_TOKEN, UNAUTHORIZED);
                 }
