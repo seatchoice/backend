6361d54ccb59c9f34e0bfaba79cbfc146d390e13
reeruryu <h2ju1004@gmail.com>
2023-02-23 16:02:07 +0900

    docs : 댓글 api 문서화
    - 댓글 요청 size max 추가
    - 댓글 조회 응답 updatedAt 패턴 변경

--- src/main/java/com/example/seatchoice/dto/request/CommentRequest.java
+++ src/main/java/com/example/seatchoice/dto/request/CommentRequest.java
@@ -1,5 +1,6 @@
 package com.example.seatchoice.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
@@ -12,23 +13,28 @@ public class CommentRequest {
 	@Getter
 	@AllArgsConstructor
 	@NoArgsConstructor
+	@Schema(name = "댓글 작성 요청")
 	public static class Create {
 
 		@NotNull(message = "필수 입력입니다.")
+		@Schema(description = "리뷰 id", example = "12")
 		private Long reviewId;
 
 		@NotBlank
-		@Size(min = 5, message = "5자 이상 댓글을 작성해 주세요.")
+		@Size(min = 5, max = 100, message = "5~100자 댓글을 작성해 주세요.")
+		@Schema(description = "댓글 내용", example = "이 자리 정말 좋네요.")
 		private String content;
 	}
 
 	@AllArgsConstructor
 	@NoArgsConstructor
 	@Getter
+	@Schema(name = "댓글 수정 요청")
 	public static class Modify {
 
 		@NotBlank
-		@Size(min = 5, message = "5자 이상 댓글을 작성해 주세요.")
+		@Size(min = 5, max = 100, message = "5~100자 댓글을 작성해 주세요.")
+		@Schema(description = "댓글 내용", example = "이 자리 정말 좋네요.")
 		private String content;
 	}
 
