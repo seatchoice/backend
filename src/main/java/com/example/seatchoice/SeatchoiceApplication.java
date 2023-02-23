e0319d2a430c7eae81935a830c39c5f14254022b
junga970 <junga2940@gmail.com>
2023-02-23 20:30:40 +0900

    fix : cors 설정

--- src/main/java/com/example/seatchoice/SeatchoiceApplication.java
+++ src/main/java/com/example/seatchoice/SeatchoiceApplication.java
@@ -3,6 +3,9 @@ package com.example.seatchoice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableFeignClients
 @SpringBootApplication
@@ -12,4 +15,13 @@ public class SeatchoiceApplication {
 		SpringApplication.run(SeatchoiceApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+			}
+		};
+	}
 }
