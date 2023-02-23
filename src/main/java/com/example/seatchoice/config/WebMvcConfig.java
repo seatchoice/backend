923c46983e60e883c203cbdb20e8146865cd30db
sunju5402 <ssonju5402@gmail.com>
2023-02-23 21:06:12 +0900

    chore : options 요청 허용

--- src/main/java/com/example/seatchoice/config/WebMvcConfig.java
+++ src/main/java/com/example/seatchoice/config/WebMvcConfig.java
@@ -12,7 +12,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
-			.allowedMethods("GET", "POST", "PUT", "DELETE")
+			.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
 			.allowedHeaders("*")
 			.allowedOriginPatterns("*")
 			.exposedHeaders("*")
