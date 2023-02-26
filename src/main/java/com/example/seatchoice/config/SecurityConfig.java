535b2f35e0e3b98fa1e72d4d5ba424040e6d1610
junga970 <junga2940@gmail.com>
2023-02-27 02:09:52 +0900

    test : spring에서 ip 허용해보기

--- src/main/java/com/example/seatchoice/config/SecurityConfig.java
+++ src/main/java/com/example/seatchoice/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import static org.springframework.http.HttpMethod.POST;
 
 import com.example.seatchoice.service.auth.TokenService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -20,6 +21,9 @@ public class SecurityConfig {
 
 	private final TokenService tokenService;
 
+	@Value("${test.ip}")
+	private String IP;
+
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http.csrf().disable()
@@ -27,6 +31,7 @@ public class SecurityConfig {
 			.and()
 			.formLogin().disable()
 			.authorizeRequests()
+			.antMatchers("/**").access("hasIpAddress('" + IP + "')")
 			.antMatchers(
 				POST,
 				"/api/oauth/{provider}/login"
