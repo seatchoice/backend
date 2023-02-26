d72a0dbd49a494722f171261c9a8e9f43c460c30
junga970 <junga2940@gmail.com>
2023-02-27 01:32:42 +0900

    test : swagger 비활성화

--- src/main/java/com/example/seatchoice/config/SwaggerConfig.java
+++ src/main/java/com/example/seatchoice/config/SwaggerConfig.java
@@ -9,8 +9,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-@Configuration
-@EnableWebMvc
+//@Configuration
+//@EnableWebMvc
 @OpenAPIDefinition(
 	info = @Info(
 		title = "자리어때 ?",
