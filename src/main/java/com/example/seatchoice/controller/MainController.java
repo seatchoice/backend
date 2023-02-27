1b48f73d358ffe13e437fad158dcc088f2c29ff2
reeruryu <h2ju1004@gmail.com>
2023-02-27 20:13:56 +0900

    chore : import 제거
    - 자동삭제가 안됐네요..

--- src/main/java/com/example/seatchoice/controller/MainController.java
+++ src/main/java/com/example/seatchoice/controller/MainController.java
@@ -2,7 +2,6 @@ package com.example.seatchoice.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 
 public class MainController {
 
