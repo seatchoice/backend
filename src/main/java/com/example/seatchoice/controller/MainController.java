2739fc02ef52dd6a9112a112b7fb3d31570d61d3
reeruryu <h2ju1004@gmail.com>
2023-02-27 21:21:15 +0900

    fix : @RestController 추가

--- src/main/java/com/example/seatchoice/controller/MainController.java
+++ src/main/java/com/example/seatchoice/controller/MainController.java
@@ -2,7 +2,9 @@ package com.example.seatchoice.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class MainController {
 
 	@GetMapping("/")
