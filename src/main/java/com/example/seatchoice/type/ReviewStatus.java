1c991ba991ca964923949e05c43791f3095193ab
sunju5402 <ssonju5402@gmail.com>
2023-03-01 22:57:18 +0900

    refact : 리뷰 코드 리팩토링 및 노란줄 코드 수정

---
+++ src/main/java/com/example/seatchoice/type/ReviewStatus.java
@@ -0,0 +1,13 @@
+package com.example.seatchoice.type;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum ReviewStatus {
+	CREATE,
+	UPDATE,
+	DELETE
+	;
+}
